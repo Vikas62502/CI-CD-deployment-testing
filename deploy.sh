d1c52700d3592b7ec7fc37d81067c9354e762111
Vikas Yadav <vikashyadav62502@gmail.com>
2024-11-12 01:55:50 +0530

    workflow updated again

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,13 @@
-cd /home/ubuntu/cm-deployment-testing
+#!/bin/bash
+
+# Navigate to the correct directory where your repository is located
+cd /home/ubuntu/climate-mitigate-backend  # Adjust as needed
+
+# Pull the latest changes from GitHub
 git pull origin main
+
+# Install any new dependencies
 npm install
+
+# Restart the application with PM2
 pm2 restart app_name || pm2 start index.js --name app_name
