cec82c3f79a3dace436a6938cfe25d8590dc2d55
vikas62502 <vikashyadav62502@gmail.com>
2024-11-15 22:28:26 +0530

    hahaha

--- index.js
+++ index.js
@@ -17,12 +17,6 @@ app.post('/check-post', (req, res) => {
     res.send("Post request");
 })
 
-app.post("/add-data-to-main", (req, res) => {
-    res.send("Data added to main");
-});
-
-
-//asdad
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 }
